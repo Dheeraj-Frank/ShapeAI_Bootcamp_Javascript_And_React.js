61432a8f7c3acd9aa96d81bcdd6ba6e8c9c19cc8
Dheeraj-Frank <85390989+Dheeraj-Frank@users.noreply.github.com>
2021-06-06 16:15:12 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+  return (
+    <footer>
+      <p1>Copyright @ 2021</p1>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+  return (
+   <div className="note">
+     <h1>Javascript and React.js</h1>
+     <p1>This was an amazing bootcamp taken up by Shaurya Sinha We covered everything from Scratch including Javascript React.js, HTML.</p1>
+     </div>
+
+  );
+}
+export default Note;
